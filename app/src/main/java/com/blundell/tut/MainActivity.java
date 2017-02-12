97c320c92683da1c809859410ad848a22db8a573
Paul Blundell <blundell.app.dev@gmail.com>
2017-02-12 09:57:40 +0000

    monitor rasp pi touch button A pin

--- app/src/main/java/com/blundell/tut/MainActivity.java
+++ app/src/main/java/com/blundell/tut/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.GpioCallback;
 import com.google.android.things.pio.PeripheralManagerService;
 
 import java.io.IOException;
@@ -33,6 +34,39 @@ public class MainActivity extends Activity {
         } catch (IOException e) {
             throw new IllegalStateException(TOUCH_BUTTON_A_PIN + " bus cannot be configured.", e);
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        try {
+            bus.setEdgeTriggerType(Gpio.EDGE_BOTH);
+            bus.registerGpioCallback(touchButtonACallback);
+        } catch (IOException e) {
+            throw new IllegalStateException(TOUCH_BUTTON_A_PIN + " bus cannot be monitored.", e);
+        }
+    }
+
+    private final GpioCallback touchButtonACallback = new GpioCallback() {
+        @Override
+        public boolean onGpioEdge(Gpio gpio) {
+            try {
+                if (gpio.getValue()) {
+                    Log.i("TUT", "ON PRESSED DOWN");
+                } else {
+                    Log.i("TUT", "ON PRESSED UP");
+                }
+            } catch (IOException e) {
+                throw new IllegalStateException(TOUCH_BUTTON_A_PIN + " cannot be read.", e);
+            }
+            return true;
+        }
+    };
+
+    @Override
+    protected void onStop() {
+        bus.unregisterGpioCallback(touchButtonACallback);
+        super.onStop();
     }
 
     @Override
