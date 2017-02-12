a84b9d2d7e0c836d41ff69167cb7e29ffb28f2a5
Paul Blundell <blundell.app.dev@gmail.com>
2017-02-12 09:57:24 +0000

    open the rasp pi touch button A pin

--- app/src/main/java/com/blundell/tut/MainActivity.java
+++ app/src/main/java/com/blundell/tut/MainActivity.java
@@ -2,11 +2,46 @@ package com.blundell.tut;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManagerService;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity {
+
+    private static final String TOUCH_BUTTON_A_PIN = "BCM21";
+
+    private Gpio bus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        PeripheralManagerService service = new PeripheralManagerService();
+
+        try {
+            bus = service.openGpio(TOUCH_BUTTON_A_PIN);
+        } catch (IOException e) {
+            throw new IllegalStateException(TOUCH_BUTTON_A_PIN + " bus cannot be opened.", e);
+        }
+
+        try {
+            bus.setDirection(Gpio.DIRECTION_IN);
+            bus.setActiveType(Gpio.ACTIVE_LOW);
+        } catch (IOException e) {
+            throw new IllegalStateException(TOUCH_BUTTON_A_PIN + " bus cannot be configured.", e);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            bus.close();
+        } catch (IOException e) {
+            Log.e("TUT", TOUCH_BUTTON_A_PIN + " bus cannot be closed, you may experience errors on next launch.", e);
+        }
     }
 }
